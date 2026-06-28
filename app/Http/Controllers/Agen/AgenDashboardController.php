296d807ee298d90048b8af904eeb4f97e9fad6ad
AndrewDonita21 <andrewdonita@gmail.com>
2026-06-28 13:35:11 +0700

    feat: tambah dashboard dasar agen level 3

--- app/Http/Controllers/Agen/AgenDashboardController.php
+++ app/Http/Controllers/Agen/AgenDashboardController.php
@@ -3,9 +3,37 @@
 namespace App\Http\Controllers\Agen;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Tiket;
 
 class AgenDashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        $total = Tiket::where('level_saat_ini', '3')->count();
+
+        $pending = Tiket::where('level_saat_ini', '3')
+                        ->where('status', 'baru')
+                        ->count();
+
+        $diproses = Tiket::where('level_saat_ini', '3')
+                        ->where('status', 'diproses')
+                        ->count();
+
+        $selesai = Tiket::where('level_saat_ini', '3')
+                        ->where('status', 'selesai')
+                        ->count();
+
+        $tikets = Tiket::where('level_saat_ini', '3')
+                        ->latest()
+                        ->take(5)
+                        ->get();
+
+        return view('agen.dashboard', compact(
+            'total',
+            'pending',
+            'diproses',
+            'selesai',
+            'tikets'
+        ));
+    }
 }
