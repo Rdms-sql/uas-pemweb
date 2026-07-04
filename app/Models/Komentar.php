4bc613b157a06a5604e95c187bbb6a96f59c2e34
AndrewDonita21 <andrewdonita@gmail.com>
2026-07-04 19:11:10 +0700

    04Jul-R3: tambah komentar, log status, dan notifikasi agen level 3

--- app/Models/Komentar.php
+++ app/Models/Komentar.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Komentar extends Model
 {
-    //
+    protected $table = 'komentars';
+
+    protected $primaryKey = 'id_komentar';
+
+    protected $fillable = [
+        'id_tiket',
+        'pengirim_tipe',
+        'id_pengirim',
+        'pesan',
+        'lampiran',
+        'waktu_kirim',
+        'is_internal',
+    ];
 }
